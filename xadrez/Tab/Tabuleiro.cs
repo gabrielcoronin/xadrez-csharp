7d507519f1e5e1aa03f926fb388cabc03766594b
Gabriel Coronin <gabrielcoronin@gmail.com>
2020-12-10 16:04:16 -0300

    Incluindo algumas exceptions

--- xadrez/Tab/Tabuleiro.cs
+++ xadrez/Tab/Tabuleiro.cs
@@ -1,4 +1,6 @@
-﻿namespace xadrez.tab
+﻿using xadrez.Tab;
+
+namespace xadrez.tab
 {
     public class Tabuleiro
     {
@@ -19,10 +21,38 @@
             return pecas[linha, coluna];
         }
 
+        public Peca Peca(Posicao posicao)
+        {
+            return pecas[posicao.Linha, posicao.Coluna];
+        }
+
+        public bool ExistePeca(Posicao posicao)
+        {
+            ValidarPosicao(posicao);
+            return Peca(posicao) != null;
+        }
+
         public void ColocarPeca(Peca peca, Posicao posicao)
         {
+            if (ExistePeca(posicao))
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+
             pecas[posicao.Linha, posicao.Coluna] = peca;
             peca.Posicao = posicao;
+        }
+
+        public bool PosicaoValida(Posicao posicao)
+        {
+            if (posicao.Linha < 0 || posicao.Linha >= Linhas || posicao.Coluna < 0 || posicao.Coluna >= Colunas)
+                return false;
+
+            return true;
+        }
+
+        public void ValidarPosicao(Posicao posicao)
+        {
+            if (!PosicaoValida(posicao))
+                throw new TabuleiroException("Posição inválida!");
         }
     }
 }
